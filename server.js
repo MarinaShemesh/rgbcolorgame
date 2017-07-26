3d209c7277ac542e838111e5eac8aa0dd10c385c
MarinaShemesh <marina.shemesh1@gmail.com>
2017-07-26 18:56:12 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+app.use(express.static('public'));
+app.use(express.static('node_modules'));
+
+app.listen(8080, function() {
+  console.log("Ears open on 8080.")
+
+});
+
